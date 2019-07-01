0ba8a9447226d9d9eb196f5fec821d311be019ad
deploymentprovectio <justone2235@gmail.com>
2019-07-01 13:55:01 +0200

    GET-IP-DEPLOYEMENT

--- roles/atera_test/files/atera.ps1
+++ roles/atera_test/files/atera.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "http://support.provect.io/GetAgent/Msi/?customerId=3&integratorLogin=clement.lerch@provectio.fr" -OutFile C:\Windows\temp\AteraAgent.msi
+Invoke-WebRequest -Uri "http://support.provect.io/GetAgent/Msi/?customerId={{ "customer_id" }}&integratorLogin=clement.lerch@provectio.fr" -OutFile C:\Windows\temp\AteraAgent.msi
